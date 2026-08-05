33bc675d44a3e246f065e590c3b679af0a6442b2
Freitaz-90 <pereira.arthur09@escola.pr.gov.br>
2026-08-05 10:16:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome;
+let numero1;
+let numero2; 
+let resultado;
+
+nome = prompt("Qual é o seu nome?");
+alert("Olá, " + nome + " seja bem-vindo a nossa calculadora de SOMA");
+//a partir daqui começa a calculadora da idade
+numero1 = 2026;
+numero2 = Number(prompt("Digite a sua data de nascimento."));
+resultado = numero1 - numero2
+alert("O resultado da sua idade é: " + resultado + " anos!");
